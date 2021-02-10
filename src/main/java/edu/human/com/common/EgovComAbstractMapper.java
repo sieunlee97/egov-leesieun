8b9decbbdf69734cf30af8dbcb762833f8ff930a
이시은 <tldms0137@naver.com>
2021-02-10 14:52:57 +0900

    20210210 5교시

--- src/main/java/edu/human/com/common/EgovComAbstractMapper.java
+++ src/main/java/edu/human/com/common/EgovComAbstractMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,10 +33,14 @@ public abstract class EgovComAbstractMapper extends EgovAbstractMapper {
 		super.setSqlSessionFactory(sqlSession);
 	}
 	
+	
+	@Override
+	public int delete(String queryId) {
+		return getSqlSession().delete(queryId);
+	}
 	@Override
 	public int delete(String queryId, Object parameterObject) {
-		// TODO Auto-generated method stub
-		return super.delete(queryId, parameterObject);
+		return getSqlSession().delete(queryId, parameterObject);
 	}
 	@Override
 	public int insert(String queryId) {
@@ -46,29 +51,48 @@ public abstract class EgovComAbstractMapper extends EgovAbstractMapper {
 		return getSqlSession().insert(queryId, parameterObject);
 	}
 	@Override
+	public <E> List<E> selectList(String queryId, Object parameterObject, RowBounds rowBounds) {
+		return getSqlSession().selectList(queryId, parameterObject, rowBounds);
+	}
+	@Override
 	public <E> List<E> selectList(String queryId, Object parameterObject) {
-		// TODO Auto-generated method stub
-		return super.selectList(queryId, parameterObject);
+		return getSqlSession().selectList(queryId, parameterObject);
 	}
 	@Override
 	public <E> List<E> selectList(String queryId) {
-		// TODO Auto-generated method stub
-		return super.selectList(queryId);
+		return  getSqlSession().selectList(queryId);
+	}
+	@Override
+	public <T> T selectOne(String queryId) {
+		return getSqlSession().selectOne(queryId);
 	}
 	@Override
 	public <T> T selectOne(String queryId, Object parameterObject) {
-		// TODO Auto-generated method stub
-		return super.selectOne(queryId, parameterObject);
+		return getSqlSession().selectOne(queryId, parameterObject);
 	}
-	
 	@Override
 	public int update(String queryId) {
 		return getSqlSession().update(queryId);
 	}
-
 	@Override
 	public int update(String queryId, Object parameterObject) {
 		return getSqlSession().update(queryId, parameterObject);
 	}
+	
+	/**
+	 * 페이징범위 계산 : pageIndex(선택한페이지)와 pageSize(한페이지당보여줄개수) 두 변수값을 매개변수로 받아서 
+	 * 계산결과1 : skipResults = pageIndex X pageSize = 선택한 페이지까지 검색된 개수
+	 * 			= 화면에 출력할 내용 중 시작할 번호 ( ex. 3페이지 클릭 - 3번*10개 =30번, 인덱스는 0번부터이기 때문에 실제 번호는 31번 )
+	 * 계산결과2 : maxResults = (선택한페이지*한페이지당보여줄개수)+한페이지당보여줄개수
+	 * 			= 화면에 출력할 내용 중 끝 번호 ( ex. 30번 + 10개 = 40번 )
+	 * queryStartNo = queryPerPageNum*(this.page-1); // 개발자가 추가한 계산식
+	 */
+	@Override
+	public List<?> listWithPaging(String queryId, Object parameterObject, int pageIndex, int pageSize) {
+		int skipResults = pageIndex * pageSize; 
+		RowBounds rowBounds = new RowBounds(skipResults, pageSize); //(시작인덱스번호, 꺼내올개수)
+		return getSqlSession().selectList(queryId, parameterObject, rowBounds);
+	}
+	
 	
 }
